5f1a6210acd2bf68830a573656cfe25ee576eb39
Hacker <133931634+Hacker-github-ctrl@users.noreply.github.com>
2023-09-24 15:45:28 -0300

    Create phishing YTB.py
    phishing

---
+++ phishing YTB.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import os
+import time
+from server import 8080
+from html import php
+from localhost import CloudFlare
+
+def main():
+        print("_______________________________")
+        print("|   PHISHING AND MALWARE       |")
+        print("|______________________________|")
+        print("            ")
+
+for in server.8080 + localhost.socket= 
+        
+        
